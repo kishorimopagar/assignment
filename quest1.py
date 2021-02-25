7bdb93813cdbb9a9c5366f08bf364a62092ec336
kishorimopagar <79629142+kishorimopagar@users.noreply.github.com>
2021-02-25 13:35:31 +0530

    Add files via upload

---
+++ quest1.py
@@ -0,0 +1,54 @@
+def fun1(S):
+    for i in range(len(S)):
+        if (S[i].isalnum()):
+            return True;
+            break;
+    return False;
+
+
+def fun2(S):
+    for i in range(len(S)):
+        if (S[i].isalpha()):
+            return True;
+            break;
+    return False;
+
+
+def fun3(S):
+    for i in range(len(S)):
+        if (S[i].isdigit()):
+            return True;
+            break;
+    return False;
+
+
+def fun4(S):
+    for i in range(len(S)):
+        if (S[i].islower()):
+            return True;
+            break;
+    return False;
+
+
+def fun5(S):
+    for i in range(len(S)):
+        if (S[i].isupper()):
+            return True;
+            break;
+    return False;
+
+
+if __name__ == '__main__':
+    S = "Kishori24"
+
+    flagalphanum = fun1(S)
+    alphabetical = fun2(S)
+    digits = fun3(S)
+    lowercase = fun4(S)
+    uppercase = fun5(S)
+    print(flagalphanum)
+    print(alphabetical)
+    print(digits)
+    print(lowercase)
+    print(uppercase)
+    
